69b491d3e2570902b3fed5a85c4bafcb7314625b
Will Kane <None>
2022-11-24 10:00:06 +0000

    Further splitting into functions.

--- src/lib.rs
+++ src/lib.rs
@@ -35,15 +35,20 @@ pub mod playing_sound {
         }
     }
 
-    pub fn play_sound() {
-        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    pub fn get_source() -> Decoder<BufReader<File>> {
         let file_path: String = super::take_args::get_path();
         let file = File::open(file_path).unwrap();
-        let sink = Sink::try_new(&stream_handle).unwrap();
         let source = Decoder::new(BufReader::new(file)).unwrap();
+        source
+    }
 
+    pub fn play_sound() {
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let source = get_source();
+        let sink = Sink::try_new(&stream_handle).unwrap();
         let configuration = extract_configuration();
 
+        // Sink initialisation and start.
         sink.set_volume(configuration.volume);
         sink.append(source);
         sink.sleep_until_end();
